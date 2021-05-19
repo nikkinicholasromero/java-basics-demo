29b997bc99f6e18a456a553ce63cf8e3fba0e062
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-05-19 05:21:49 -0400

    Generics

---
+++ src/com/demo/Generics.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        List<Number> numbers01 = new ArrayList<Number>();
+        // List<Number> numbers02 = new ArrayList<Integer>(); // doesn't compile
+        List<Number> numbers03 = new ArrayList<>();
+        List<Number> numbers04 = new ArrayList();
+
+        // List<> numbers05 = new ArrayList(); // doesn't compile
+        List numbers05 = new ArrayList<Number>();
+        List numbers06 = new ArrayList<>();
+        List numbers07 = new ArrayList();
+    }
+}
