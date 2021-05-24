ab3053ded9af12c4f96133fe6555c71e05beb2ed
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-05-24 15:23:08 -0400

    Searching and sorting

--- src/com/demo/ArrayAndList.java
+++ src/com/demo/ArrayAndList.java
@@ -1,12 +1,10 @@
 package com.demo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ArrayAndList {
     public static void main(String[] args) {
-
+        sortingAndSearching();
     }
 
     private static void arrayToList() {
@@ -23,5 +21,22 @@ public class ArrayAndList {
 
         String[] namesArray0 = namesList.toArray(new String[0]);
         String[] namesArray1 = (String[]) namesList.toArray();
+
+        // java.util.Collection; <-- Interface
+        // java.util.Collections; <-- Class
+    }
+
+    private static void sortingAndSearching() {
+        String[] namesArray = { "Nikki", "Leslie", "Maven", "Megan" };
+        Arrays.sort(namesArray);
+        System.out.println(Arrays.binarySearch(namesArray, "Maven"));
+
+        List<String> namesList = new ArrayList<>();
+        namesList.add("Nikki");
+        namesList.add("Leslie");
+        namesList.add("Maven");
+        namesList.add("Megan");
+        Collections.sort(namesList);
+        System.out.println(Collections.binarySearch(namesList, "Megan"));
     }
 }
