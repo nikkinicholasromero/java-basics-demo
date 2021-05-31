5b3bdb264b45df1b100d718292556ff90b1e861d
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-05-30 21:32:18 -0400

    Streams

--- src/com/demo/Streams.java
+++ src/com/demo/Streams.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 import java.util.Collections;
+import java.util.function.*;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -12,6 +13,7 @@ public class Streams {
         names.sorted(Collections.reverseOrder()).forEach(System.out::println);
 
         primitiveStreams();
+        convertingStreams();
     }
 
     private static void primitiveStreams() {
@@ -38,5 +40,87 @@ public class Streams {
         DoubleStream doubleStream04 = DoubleStream.iterate(1, (a) -> a + 1);
         // DoubleStream doubleStream05 = DoubleStream.range(1, 6); // DoubleStream does not have range method
         // DoubleStream doubleStream06 = DoubleStream.rangeClosed(1, 5); // DoubleStream does not have rangeClosed method
+    }
+
+    private static void convertingStreams() {
+        Stream<String> streamToStreamSource = Stream.of("Nikki", "Leslie", "Maven", "Megan");
+        Function<String, String> streamToStreamMapper = String::toUpperCase;
+        Stream<String> streamToStreamOutput = streamToStreamSource.map(streamToStreamMapper);
+        streamToStreamOutput.forEach(System.out::println);
+
+        Stream<String> streamToIntSource = Stream.of("Nikki", "Leslie", "Maven", "Megan");
+        ToIntFunction<String> streamToIntMapper = String::length;
+        IntStream streamToIntOutput = streamToIntSource.mapToInt(streamToIntMapper);
+        streamToIntOutput.forEach(System.out::println);
+
+        Stream<String> streamToLongSource = Stream.of("Nikki", "Leslie", "Maven", "Megan");
+        ToLongFunction<String> streamToLongMapper = String::length;
+        LongStream streamToLongOutput = streamToLongSource.mapToLong(streamToLongMapper);
+        streamToLongOutput.forEach(System.out::println);
+
+        Stream<String> streamToDoubleSource = Stream.of("Nikki", "Leslie", "Maven", "Megan");
+        ToDoubleFunction<String> streamToDoubleMapper = (s) -> s.length() / 1.5;
+        DoubleStream streamToDoubleOutput = streamToDoubleSource.mapToDouble(streamToDoubleMapper);
+        streamToDoubleOutput.forEach(System.out::println);
+
+        IntStream intStreamToStreamSource = IntStream.of(1, 2, 3, 4);
+        IntFunction<String> intStreamToStreamMapper = String::valueOf;
+        Stream<String> intStreamToStreamOutput = intStreamToStreamSource.mapToObj(intStreamToStreamMapper);
+        intStreamToStreamOutput.forEach(System.out::println);
+
+        IntStream intStreamToIntStreamSource = IntStream.of(1, 2, 3, 4);
+        IntUnaryOperator intStreamToIntStreamMapper = (v) -> v * v;
+        IntStream intStreamToIntStreamOutput = intStreamToIntStreamSource.map(intStreamToIntStreamMapper);
+        intStreamToIntStreamOutput.forEach(System.out::println);
+
+        IntStream intStreamToLongStreamSource = IntStream.of(1, 2, 3, 4);
+        IntToLongFunction intStreamToLongStreamMapper = (v) -> v * v;
+        LongStream intStreamToLongStreamOutput = intStreamToLongStreamSource.mapToLong(intStreamToLongStreamMapper);
+        intStreamToLongStreamOutput.forEach(System.out::println);
+
+        IntStream intStreamToDoubleStreamSource = IntStream.of(1, 2, 3, 4);
+        IntToDoubleFunction intStreamToDoubleStreamMapper = (v) -> v * v;
+        DoubleStream intStreamToDoubleStreamOutput = intStreamToDoubleStreamSource.mapToDouble(intStreamToDoubleStreamMapper);
+        intStreamToDoubleStreamOutput.forEach(System.out::println);
+
+        LongStream longStreamToStreamSource = LongStream.of(1, 2, 3, 4);
+        LongFunction<String> longStreamToStreamMapper = String::valueOf;
+        Stream<String> longStreamToStreamOutput = longStreamToStreamSource.mapToObj(longStreamToStreamMapper);
+        longStreamToStreamOutput.forEach(System.out::println);
+
+        LongStream longStreamToIntStreamSource = LongStream.of(1, 2, 3, 4);
+        LongToIntFunction longStreamToIntStreamMapper = (v) -> (int) v;
+        IntStream longStreamToIntStreamOutput = longStreamToIntStreamSource.mapToInt(longStreamToIntStreamMapper);
+        longStreamToIntStreamOutput.forEach(System.out::println);
+
+        LongStream longStreamToLongStreamSource = LongStream.of(1, 2, 3, 4);
+        LongUnaryOperator longStreamToLongStreamMapper = (v) -> v * v;
+        LongStream longStreamToLongStreamOutput = longStreamToLongStreamSource.map(longStreamToLongStreamMapper);
+        longStreamToLongStreamOutput.forEach(System.out::println);
+
+        LongStream longStreamToDoubleStreamSource = LongStream.of(1, 2, 3, 4);
+        LongToDoubleFunction longStreamToDoubleStreamMapper = (v) -> v / 1.5;
+        DoubleStream longStreamToDoubleStreamOutput = longStreamToDoubleStreamSource.mapToDouble(longStreamToDoubleStreamMapper);
+        longStreamToDoubleStreamOutput.forEach(System.out::println);
+
+        DoubleStream doubleStreamToStreamSource = DoubleStream.of(1.0, 2.0, 3.0, 4.0);
+        DoubleFunction<String> doubleStreamToStreamMapper = String::valueOf;
+        Stream<String> doubleStreamToStreamOutput = doubleStreamToStreamSource.mapToObj(doubleStreamToStreamMapper);
+        doubleStreamToStreamOutput.forEach(System.out::println);
+
+        DoubleStream doubleStreamToIntStreamSource = DoubleStream.of(1.0, 2.0, 3.0, 4.0);
+        DoubleToIntFunction doubleStreamToIntStreamMapper = (v) -> (int) v;
+        IntStream doubleStreamToIntStreamOutput = doubleStreamToIntStreamSource.mapToInt(doubleStreamToIntStreamMapper);
+        doubleStreamToIntStreamOutput.forEach(System.out::println);
+
+        DoubleStream doubleStreamToLongStreamSource = DoubleStream.of(1.0, 2.0, 3.0, 4.0);
+        DoubleToLongFunction doubleStreamToLongStreamMapper = (v) -> (long) v;
+        LongStream doubleStreamToLongStreamOutput = doubleStreamToLongStreamSource.mapToLong(doubleStreamToLongStreamMapper);
+        doubleStreamToLongStreamOutput.forEach(System.out::println);
+
+        DoubleStream doubleStreamToDoubleStreamSource = DoubleStream.of(1.0, 2.0, 3.0, 4.0);
+        DoubleUnaryOperator doubleStreamToDoubleStreamMapper = (v) -> v * v;
+        DoubleStream doubleStreamToDoubleStreamOutput = doubleStreamToDoubleStreamSource.map(doubleStreamToDoubleStreamMapper);
+        doubleStreamToDoubleStreamOutput.forEach(System.out::println);
     }
 }
